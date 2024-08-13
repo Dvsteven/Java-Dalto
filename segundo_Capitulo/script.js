c2c3ba793f878ab5b8d0830875906f491fd023d4
Steven <stevenhenao81@gamil.com>
2024-08-13 12:29:14 -0500

    Avance segundo capitulo 3h30m

--- segundo_Capitulo/script.js
+++ segundo_Capitulo/script.js
@@ -75,10 +75,109 @@
 // }
 
 //----------------------------------------------------------------
+
 //**for
-//**for in
+//Composicion: variable - condicion - iteracion (aumento o decremento)
+// for (let i = 0; i <= 6; i++){
+//     document.write(i + "<br>")
+// }
+
+//----------------------------------------------------------------
+
+//**continue
+// for (let i = 0; i <= 6; i++){
+//     if (i == 3){
+//         continue;
+//     }
+//     document.write(i + "<br>")
+
+// }
+
+//----------------------------------------------------------------
+
+
+// //**for in
+// let list = ["gato","perro","pez","lombriz"]
+// for (animal in list){
+//     document.write(animal + "<br>") //Muestra los indices (posicion)
+// }
+// document.write("<br>")
+// //si primero hago referencia a la lista luego a la variable que almacena el indice, aparece el nombre de esa posicion
+// //lo cual seria equivalente a esto: list[0] 
+// for (animal in list){
+//     document.write(list[animal] + "<br>")
+// }
+
+// document.write("<br>")
 //**for of
-//**----------
+// for (animal of list){
+//     document.write(animal + "<br>")//Muestra el contenido de la lista
+// }
+
+
+
+//**-------------------------------------------------------------
 //**break
 //**label
-//**continue
+
+
+// array1 = ["maria","Josefa","roberta"];
+// array2 = ["rogelio","roger",array1,"pedrou"];
+
+// forRancio:
+// for (let array in array2){
+//     if (array == 2){
+//         for (let array of array1){
+//             if (array == "maria"){
+//                 continue forRancio; //se satla esta iteracion y regresa al "label: forRancio"
+//             }
+//             document.write(array + "<br>");
+//         }
+//     }else {
+//         document.write(array2[array] + "<br>");
+//     }
+// }
+
+//**-------------------------------------------------------------
+//**Funciones
+
+//Se puede almacenar tambien como variable
+
+// function saludar(){
+//     respuesta = prompt("Hola, que bueno es saludar y ser saludado: ");
+//     if (respuesta == "hola"){
+//         alert("Muchas gracias")
+//     }else{
+//         alert("Que bueno es saludar Y SER SALUDADO")
+//     }
+// }
+
+// saludar()
+ //**---------------------------------------------------------
+//  function sumar(num1,num2){
+//     let res = num1 + num2; //si no le pongo let, el scope hace que sea una variable usable en cualquier parte del codigo y no solo dentro de la funcion
+//     return res;
+//  }
+
+// let resultado = sumar(13, 31)
+// //siempre que se le pasan distintos PARAMETROS realiza distintas operaciones
+// // sumar(12, 12)
+// // sumar(89, 45)
+
+//formas de crear las funciones:
+
+// function saludar(){
+//     document.write("Hola!");
+// }
+
+// const saludar = function(parametro){
+//     let frase = `Hey ${parametro}`;
+//     document.write(frase);
+// }
+
+// const saludar = (parametro)=>{
+//     let frase = `Hey ${parametro}`;
+//     document.write(frase);
+// }
+
+// saludar("Hola!") //se ñe pasa este parametro solo a los dos ultimos ejemplos ↑
